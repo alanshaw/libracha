4dd73f6ee09e9ad4aa921eb62e23288c32e4163e
Alan Shaw <alan138@gmail.com>
2026-04-28 12:06:38 +0100

    fix: remove provider admin caps

--- capabilities/provider/datamodel/deregister.go
+++
@@ -1,9 +0,0 @@
-package datamodel
-
-import (
-	"github.com/alanshaw/ucantone/did"
-)
-
-type DeregisterArgumentsModel struct {
-	Provider did.DID `cborgen:"provider" dagjsongen:"provider"`
-}
--- capabilities/provider/weight/datamodel/set.go
+++
@@ -1,8 +0,0 @@
-package datamodel
-
-import "github.com/alanshaw/ucantone/did"
-
-type SetArgumentsModel struct {
-	Provider did.DID `cborgen:"provider" dagjsongen:"provider"`
-	Weight   uint64  `cborgen:"weight" dagjsongen:"weight"`
-}
