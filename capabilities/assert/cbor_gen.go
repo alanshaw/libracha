fil-forge/libforge

refactor: rename location commitment range fields

capabilities/assert/datamodel/cbor_gen.go
@@ -372,7 +372,7 @@ func (t *RangeModel) MarshalCBOR(w io.Writer) error {
 	cw := cbg.NewCborWriter(w)
 	fieldCount := 2
 
-	if t.Length == nil {
+	if t.End == nil {
 		fieldCount--
 	}
 
@@ -380,46 +380,58 @@ func (t *RangeModel) MarshalCBOR(w io.Writer) error {
 		return err
 	}
 
-	// t.Length (uint64) (uint64)
-	if t.Length != nil {
+	// t.End (int64) (int64)
+	if t.End != nil {
 
-		if len("length") > 8192 {
-			return xerrors.Errorf("Value in field \"length\" was too long")
+		if len("end") > 8192 {
+			return xerrors.Errorf("Value in field \"end\" was too long")
 		}
 
-		if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("length"))); err != nil {
+		if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("end"))); err != nil {
 			return err
 		}
-		if _, err := cw.WriteString(string("length")); err != nil {
+		if _, err := cw.WriteString(string("end")); err != nil {
 			return err
 		}
 
-		if t.Length == nil {
+		if t.End == nil {
 			if _, err := cw.Write(cbg.CborNull); err != nil {
 				return err
 			}
 		} else {
-			if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(*t.Length)); err != nil {
-				return err
+			if *t.End >= 0 {
+				if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(*t.End)); err != nil {
+					return err
+				}
+			} else {
+				if err := cw.WriteMajorTypeHeader(cbg.MajNegativeInt, uint64(-*t.End-1)); err != nil {
+					return err
+				}
 			}
 		}
 
 	}
 
-	// t.Offset (uint64) (uint64)
-	if len("offset") > 8192 {
-		return xerrors.Errorf("Value in field \"offset\" was too long")
+	// t.Start (int64) (int64)
+	if len("start") > 8192 {
+		return xerrors.Errorf("Value in field \"start\" was too long")
 	}
 
-	if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("offset"))); err != nil {
+	if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("start"))); err != nil {
 		return err
 	}
-	if _, err := cw.WriteString(string("offset")); err != nil {
+	if _, err := cw.WriteString(string("start")); err != nil {
 		return err
 	}
 
-	if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(t.Offset)); err != nil {
-		return err
+	if t.Start >= 0 {
+		if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(t.Start)); err != nil {
+			return err
+		}
+	} else {
+		if err := cw.WriteMajorTypeHeader(cbg.MajNegativeInt, uint64(-t.Start-1)); err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -450,7 +462,7 @@ func (t *RangeModel) UnmarshalCBOR(r io.Reader) (err error) {
 
 	n := extra
 
-	nameBuf := make([]byte, 6)
+	nameBuf := make([]byte, 5)
 	for i := uint64(0); i < n; i++ {
 		nameLen, ok, err := cbg.ReadFullStringIntoBuf(cr, nameBuf, 8192)
 		if err != nil {
@@ -466,9 +478,8 @@ func (t *RangeModel) UnmarshalCBOR(r io.Reader) (err error) {
 		}
 
 		switch string(nameBuf[:nameLen]) {
-		// t.Length (uint64) (uint64)
-		case "length":
-
+		// t.End (int64) (int64)
+		case "end":
 			{
 
 				b, err := cr.ReadByte()
@@ -479,32 +490,55 @@ func (t *RangeModel) UnmarshalCBOR(r io.Reader) (err error) {
 					if err := cr.UnreadByte(); err != nil {
 						return err
 					}
-					maj, extra, err = cr.ReadHeader()
+					maj, extra, err := cr.ReadHeader()
 					if err != nil {
 						return err
 					}
-					if maj != cbg.MajUnsignedInt {
-						return fmt.Errorf("wrong type for uint64 field")
+					var extraI int64
+					switch maj {
+					case cbg.MajUnsignedInt:
+						extraI = int64(extra)
+						if extraI < 0 {
+							return fmt.Errorf("int64 positive overflow")
+						}
+					case cbg.MajNegativeInt:
+						extraI = int64(extra)
+						if extraI < 0 {
+							return fmt.Errorf("int64 negative overflow")
+						}
+						extraI = -1 - extraI
+					default:
+						return fmt.Errorf("wrong type for int64 field: %d", maj)
 					}
-					typed := uint64(extra)
-					t.Length = &typed
+
+					t.End = (*int64)(&extraI)
 				}
-
 			}
-			// t.Offset (uint64) (uint64)
-		case "offset":
-
+			// t.Start (int64) (int64)
+		case "start":
 			{
-
-				maj, extra, err = cr.ReadHeader()
+				maj, extra, err := cr.ReadHeader()
 				if err != nil {
 					return err
 				}
-				if maj != cbg.MajUnsignedInt {
-					return fmt.Errorf("wrong type for uint64 field")
+				var extraI int64
+				switch maj {
+				case cbg.MajUnsignedInt:
+					extraI = int64(extra)
+					if extraI < 0 {
+						return fmt.Errorf("int64 positive overflow")
+					}
+				case cbg.MajNegativeInt:
+					extraI = int64(extra)
+					if extraI < 0 {
+						return fmt.Errorf("int64 negative overflow")
+					}
+					extraI = -1 - extraI
+				default:
+					return fmt.Errorf("wrong type for int64 field: %d", maj)
 				}
-				t.Offset = uint64(extra)
 
+				t.Start = int64(extraI)
 			}
 
 		default:

capabilities/assert/datamodel/json_gen.go
@@ -385,25 +385,25 @@ func (t *RangeModel) MarshalDagJSON(w io.Writer) error {
 	}
 	written := 0
 
-	// t.Length (uint64) (uint64)
-	if t.Length != nil {
-		if len("length") > 8192 {
-			return fmt.Errorf("string in field \"length\" was too long")
+	// t.End (int64) (int64)
+	if t.End != nil {
+		if len("end") > 8192 {
+			return fmt.Errorf("string in field \"end\" was too long")
 		}
-		if err := jw.WriteString(string("length")); err != nil {
-			return fmt.Errorf("writing string for field \"length\": %w", err)
+		if err := jw.WriteString(string("end")); err != nil {
+			return fmt.Errorf("writing string for field \"end\": %w", err)
 		}
 		if err := jw.WriteObjectColon(); err != nil {
 			return err
 		}
 
-		if t.Length == nil {
+		if t.End == nil {
 			if err := jw.WriteNull(); err != nil {
-				return fmt.Errorf("writing null for field t.Length: %w", err)
+				return fmt.Errorf("writing null for field t.End: %w", err)
 			}
 		} else {
-			if err := jw.WriteUint64(uint64(*t.Length)); err != nil {
-				return fmt.Errorf("writing uint64 for field t.Length: %w", err)
+			if err := jw.WriteInt64(int64(*t.End)); err != nil {
+				return fmt.Errorf("writing int64 for field t.End: %w", err)
 			}
 		}
 
@@ -415,19 +415,19 @@ func (t *RangeModel) MarshalDagJSON(w io.Writer) error {
 		}
 	}
 
-	// t.Offset (uint64) (uint64)
-	if len("offset") > 8192 {
-		return fmt.Errorf("string in field \"offset\" was too long")
+	// t.Start (int64) (int64)
+	if len("start") > 8192 {
+		return fmt.Errorf("string in field \"start\" was too long")
 	}
-	if err := jw.WriteString(string("offset")); err != nil {
-		return fmt.Errorf("writing string for field \"offset\": %w", err)
+	if err := jw.WriteString(string("start")); err != nil {
+		return fmt.Errorf("writing string for field \"start\": %w", err)
 	}
 	if err := jw.WriteObjectColon(); err != nil {
 		return err
 	}
 
-	if err := jw.WriteUint64(uint64(t.Offset)); err != nil {
-		return fmt.Errorf("writing uint64 for field t.Offset: %w", err)
+	if err := jw.WriteInt64(int64(t.Start)); err != nil {
+		return fmt.Errorf("writing int64 for field t.Start: %w", err)
 	}
 
 	written++
@@ -470,30 +470,30 @@ func (t *RangeModel) UnmarshalDagJSON(r io.Reader) (err error) {
 			}
 			switch name {
 
-			// t.Length (uint64) (uint64)
-			case "length":
+			// t.End (int64) (int64)
+			case "end":
 				{
 
-					nval, err := jr.ReadNumberAsUint64OrNull()
+					nval, err := jr.ReadNumberAsInt64OrNull()
 					if err != nil {
-						return fmt.Errorf("reading uint64 or null for field t.Length: %w", err)
+						return fmt.Errorf("reading int64 or null for field t.End: %w", err)
 					}
 					if nval != nil {
-						typed := uint64(*nval)
-						t.Length = &typed
+						typed := int64(*nval)
+						t.End = &typed
 					}
 
 				}
 
-				// t.Offset (uint64) (uint64)
-			case "offset":
+				// t.Start (int64) (int64)
+			case "start":
 				{
 
-					nval, err := jr.ReadNumberAsUint64()
+					nval, err := jr.ReadNumberAsInt64()
 					if err != nil {
-						return fmt.Errorf("reading uint64 for field t.Offset: %w", err)
+						return fmt.Errorf("reading int64 for field t.Start: %w", err)
 					}
-					t.Offset = uint64(nval)
+					t.Start = int64(nval)
 
 				}
 			default:

alanshaw/libracha

capabilities/assert/cbor_gen.go
@@ -374,7 +374,7 @@ func (t *Range) MarshalCBOR(w io.Writer) error {
 	cw := cbg.NewCborWriter(w)
 	fieldCount := 2
 
-	if t.Length == nil {
+	if t.End == nil {
 		fieldCount--
 	}
 
@@ -382,45 +382,45 @@ func (t *Range) MarshalCBOR(w io.Writer) error {
 		return err
 	}
 
-	// t.Length (uint64) (uint64)
-	if t.Length != nil {
+	// t.End (uint64) (uint64)
+	if t.End != nil {
 
-		if len("length") > 8192 {
-			return xerrors.Errorf("Value in field \"length\" was too long")
+		if len("end") > 8192 {
+			return xerrors.Errorf("Value in field \"end\" was too long")
 		}
 
-		if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("length"))); err != nil {
+		if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("end"))); err != nil {
 			return err
 		}
-		if _, err := cw.WriteString(string("length")); err != nil {
+		if _, err := cw.WriteString(string("end")); err != nil {
 			return err
 		}
 
-		if t.Length == nil {
+		if t.End == nil {
 			if _, err := cw.Write(cbg.CborNull); err != nil {
 				return err
 			}
 		} else {
-			if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(*t.Length)); err != nil {
+			if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(*t.End)); err != nil {
 				return err
 			}
 		}
 
 	}
 
-	// t.Offset (uint64) (uint64)
-	if len("offset") > 8192 {
-		return xerrors.Errorf("Value in field \"offset\" was too long")
+	// t.Start (uint64) (uint64)
+	if len("start") > 8192 {
+		return xerrors.Errorf("Value in field \"start\" was too long")
 	}
 
-	if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("offset"))); err != nil {
+	if err := cw.WriteMajorTypeHeader(cbg.MajTextString, uint64(len("start"))); err != nil {
 		return err
 	}
-	if _, err := cw.WriteString(string("offset")); err != nil {
+	if _, err := cw.WriteString(string("start")); err != nil {
 		return err
 	}
 
-	if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(t.Offset)); err != nil {
+	if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(t.Start)); err != nil {
 		return err
 	}
 
@@ -452,7 +452,7 @@ func (t *Range) UnmarshalCBOR(r io.Reader) (err error) {
 
 	n := extra
 
-	nameBuf := make([]byte, 6)
+	nameBuf := make([]byte, 5)
 	for i := uint64(0); i < n; i++ {
 		nameLen, ok, err := cbg.ReadFullStringIntoBuf(cr, nameBuf, 8192)
 		if err != nil {
@@ -468,8 +468,8 @@ func (t *Range) UnmarshalCBOR(r io.Reader) (err error) {
 		}
 
 		switch string(nameBuf[:nameLen]) {
-		// t.Length (uint64) (uint64)
-		case "length":
+		// t.End (uint64) (uint64)
+		case "end":
 
 			{
 
@@ -489,12 +489,12 @@ func (t *Range) UnmarshalCBOR(r io.Reader) (err error) {
 						return fmt.Errorf("wrong type for uint64 field")
 					}
 					typed := uint64(extra)
-					t.Length = &typed
+					t.End = &typed
 				}
 
 			}
-			// t.Offset (uint64) (uint64)
-		case "offset":
+			// t.Start (uint64) (uint64)
+		case "start":
 
 			{
 
@@ -505,7 +505,7 @@ func (t *Range) UnmarshalCBOR(r io.Reader) (err error) {
 				if maj != cbg.MajUnsignedInt {
 					return fmt.Errorf("wrong type for uint64 field")
 				}
-				t.Offset = uint64(extra)
+				t.Start = uint64(extra)
 
 			}
 

capabilities/assert/json_gen.go
@@ -387,25 +387,25 @@ func (t *Range) MarshalDagJSON(w io.Writer) error {
 	}
 	written := 0
 
-	// t.Length (uint64) (uint64)
-	if t.Length != nil {
-		if len("length") > 8192 {
-			return fmt.Errorf("string in field \"length\" was too long")
+	// t.End (uint64) (uint64)
+	if t.End != nil {
+		if len("end") > 8192 {
+			return fmt.Errorf("string in field \"end\" was too long")
 		}
-		if err := jw.WriteString(string("length")); err != nil {
-			return fmt.Errorf("writing string for field \"length\": %w", err)
+		if err := jw.WriteString(string("end")); err != nil {
+			return fmt.Errorf("writing string for field \"end\": %w", err)
 		}
 		if err := jw.WriteObjectColon(); err != nil {
 			return err
 		}
 
-		if t.Length == nil {
+		if t.End == nil {
 			if err := jw.WriteNull(); err != nil {
-				return fmt.Errorf("writing null for field t.Length: %w", err)
+				return fmt.Errorf("writing null for field t.End: %w", err)
 			}
 		} else {
-			if err := jw.WriteUint64(uint64(*t.Length)); err != nil {
-				return fmt.Errorf("writing uint64 for field t.Length: %w", err)
+			if err := jw.WriteUint64(uint64(*t.End)); err != nil {
+				return fmt.Errorf("writing uint64 for field t.End: %w", err)
 			}
 		}
 
@@ -417,19 +417,19 @@ func (t *Range) MarshalDagJSON(w io.Writer) error {
 		}
 	}
 
-	// t.Offset (uint64) (uint64)
-	if len("offset") > 8192 {
-		return fmt.Errorf("string in field \"offset\" was too long")
+	// t.Start (uint64) (uint64)
+	if len("start") > 8192 {
+		return fmt.Errorf("string in field \"start\" was too long")
 	}
-	if err := jw.WriteString(string("offset")); err != nil {
-		return fmt.Errorf("writing string for field \"offset\": %w", err)
+	if err := jw.WriteString(string("start")); err != nil {
+		return fmt.Errorf("writing string for field \"start\": %w", err)
 	}
 	if err := jw.WriteObjectColon(); err != nil {
 		return err
 	}
 
-	if err := jw.WriteUint64(uint64(t.Offset)); err != nil {
-		return fmt.Errorf("writing uint64 for field t.Offset: %w", err)
+	if err := jw.WriteUint64(uint64(t.Start)); err != nil {
+		return fmt.Errorf("writing uint64 for field t.Start: %w", err)
 	}
 
 	written++
@@ -472,30 +472,30 @@ func (t *Range) UnmarshalDagJSON(r io.Reader) (err error) {
 			}
 			switch name {
 
-			// t.Length (uint64) (uint64)
-			case "length":
+			// t.End (uint64) (uint64)
+			case "end":
 				{
 
 					nval, err := jr.ReadNumberAsUint64OrNull()
 					if err != nil {
-						return fmt.Errorf("reading uint64 or null for field t.Length: %w", err)
+						return fmt.Errorf("reading uint64 or null for field t.End: %w", err)
 					}
 					if nval != nil {
 						typed := uint64(*nval)
-						t.Length = &typed
+						t.End = &typed
 					}
 
 				}
 
-				// t.Offset (uint64) (uint64)
-			case "offset":
+				// t.Start (uint64) (uint64)
+			case "start":
 				{
 
 					nval, err := jr.ReadNumberAsUint64()
 					if err != nil {
-						return fmt.Errorf("reading uint64 for field t.Offset: %w", err)
+						return fmt.Errorf("reading uint64 for field t.Start: %w", err)
 					}
-					t.Offset = uint64(nval)
+					t.Start = uint64(nval)
 
 				}
 			default: